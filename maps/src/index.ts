e6bb374169ee0e089d27707fe213b3f0605d812a
Caden <caden92@outlook.com>
2022-05-31 12:41:37 +1000

    pop up

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -4,9 +4,8 @@ import { Company } from './Company';
 import { CustomMap } from './CustomMap';
 
 const user = new User();
-console.log(user);
-
 const company = new Company();
-console.log(company);
+const customMap = new CustomMap('map');
 
-new CustomMap('map');
+customMap.addMarker(user);
+customMap.addMarker(company);
